d9db01599b60113c8636368a114587879b5f91d6
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2018-03-02 17:34:46 -0500

    Add configurations of the web application, including setting up ViewResolver, ResourceHandler, servlet mapping and ComponentScan.

---
+++ src/main/java/com/twl/xg/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.twl.xg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.twl.xg.controller", "com.twl.xg.service"})
+public class AppConfig implements WebMvcConfigurer {
+  @Bean
+  public ViewResolver jspViewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setViewClass(JstlView.class);
+    resolver.setPrefix("/WEB-IFN/jsp/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+  }
+}
