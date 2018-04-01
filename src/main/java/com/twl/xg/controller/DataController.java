f9eed2af8730e4a386e1f4dba872952f12c5e6f6
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2018-04-01 18:43:03 -0400

    Implementing dao Update config class to support annotation "@Transaction" in service classes. Finish implementation of dao: "BorderRouterRepository", "SensorDataRepository" and "SensorRepository" class. Also write unit test for these classes.

--- src/main/java/com/twl/xg/controller/DataController.java
+++ src/main/java/com/twl/xg/controller/DataController.java
@@ -8,7 +8,6 @@ import com.twl.xg.service.DataFetchingAndMappingService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
